50d88871e9c16086b88d0e00b2e76e7334fe0061
Zhenya_01 <zhenya170401@gmail.com>
2020-11-22 16:52:31 +0500

    Adding Movie details screen

--- app/src/main/java/com/zhenya/zhenyaFundamentalsProject/MainActivity.kt
+++ app/src/main/java/com/zhenya/zhenyaFundamentalsProject/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.zhenya.zhenyaFundamentalsProject
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textView = findViewById<TextView>(R.id.textView)
+        textView.setOnClickListener { goToMovies(this) }
+    }
+
+    fun goToMovies(context: Context){
+        val nextIntent = Intent(context, MovieDetailsActivity::class.java)
+        startActivity(nextIntent)
     }
 }
